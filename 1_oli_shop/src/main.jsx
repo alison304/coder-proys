e8b4038deb74ab491ddd378d43d28dbad4f26ca5
Alison Janise Paico Quispe <alison.jpq@gmail.com>
2023-04-29 22:00:12 -0500

    fixed

--- 1_oli_shop/src/main.jsx
+++ 1_oli_shop/src/main.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />} />
-    </Routes>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<ItemListContainer />} />
+        <Route path='/category/:categoryId' element={<ItemListContainer/>} />
+        <Route path='/item/:itemId' element={<ItemDetailContainer/>} />
+        <Route path='*' element={<h1>404 NOT FOUND</h1>} />
+      </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
